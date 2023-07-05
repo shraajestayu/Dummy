938fb255f7ffc5f3d3e0ec250f795e82c319fa38
AYUSH_ASH <107471238+shraajestayu@users.noreply.github.com>
2023-07-05 15:12:12 +0530

    Delete promo directory

--- promo/promoinfo.py
+++
@@ -1,3 +0,0 @@
-promo_photo="/promo/photo.jpg"
-
-promo_text="📣📣📣📣📣📣📣\n🔔🔔🔔🔔🔔🔔🔔\n𝗦𝘁𝗼𝗽 𝘄𝗮𝘀𝘁𝗶𝗻𝗴 𝘆𝗼𝘂𝗿 𝗽𝗿𝗲𝗰𝗶𝗼𝘂𝘀 𝘁𝗶𝗺𝗲 𝗲𝗮𝗿𝗻 𝗱𝗮𝗶𝗹𝘆 3000+ 𝗯𝘆 𝗽𝗹𝗮𝘆𝗶𝗻𝗴 𝘀𝗶𝗺𝗽𝗹𝗲 𝗴𝗮𝗺𝗲𝘀\n𝗖𝗼𝗻𝘁𝗮𝗰𝘁 ~ @Arnavtc\n\n➡️𝗝𝗼𝗶𝗻 ~ https://t.me/tcwitharnav\n👉EARN MONEY WITH ME\n👉DAILY RS.3000\n👉VERY EASY\n👉JUST JOIN V\n☄️ minimum recharge -100\n⚡️ minimum withdrawal -110\n❤️TEXT ME - @arnavtc\n📌REGISTER HERE THEN SEND ME SCREENSHOT AFTER LOGIN\nhttps://www.9987up.vip/#/register?r_code=cD7QK653493\n\n❤️‍🔥JOIN - https://t.me/tcwitharnav\n✅✅✅✅✅✅"
